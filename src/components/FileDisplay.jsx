188ca765384e1d49e2264e861af2c0c8770594ad
Lachlan <lachlan.camp.lc@gmail.com>
2025-03-10 14:51:41 +1100

    Added record audio

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -3,7 +3,16 @@ import React from "react"
 export default function FileDisplay(props) {
     const { handleAudioReset, file, audioStream } = props
     return (
-        <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center text-center pb-20'>
+        <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 justify-center text-center pb-20 w-fit max-w-full mx-auto'>
+            <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Your <span className='text-blue-400 bold'>File</span></h1>
+            <div className="mx-auto flex flex-col text-left my-4">
+                <h3 className="font-semibold">Name</h3>
+                <p>{file ? file?.name : 'Custom Audio'}</p>
+                <div onClick={handleAudioReset} className="flex items-center justify-between gap-4 hover:text-blue-600 duration-200">
+                    <button className="text-slate-400">Reset</button>
+                    <button className="specialBtn p-2 px-3 rounded-lg text-blue-400 flex items-center gap-2 font-medium"><p>Transcribe</p><i className="fa-solid fa-pen-nib"></i></button>
+                </div>
+            </div>
         </main>
     )
 };

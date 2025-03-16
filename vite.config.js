a5bb1ff2f5f0077817335c053ecbb8e36e79de3c
Lachlan <lachlan.camp.lc@gmail.com>
2025-03-17 09:22:36 +1100

    test fix for netlify

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['@xenova/transformers']
+    }
+  },
   plugins: [
     tailwindcss(), react()
   ],
 })
+
+
+
+
+
+
+/* export default defineConfig({
+  plugins: [
+    tailwindcss(), react()
+  ],
+}) */
